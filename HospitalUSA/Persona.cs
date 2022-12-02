a32496166af8dc7a82fdb3d9fd16492d762ec761
APSMarcCliment <marc.climent@airplane.solutions>
2022-12-02 12:05:59 +0100

    First commit class persona

---
+++ HospitalUSA/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalUSA
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public Persona() { }
+
+        public string getNombre() { return nombre; }
+        public int getEdad() { return edad;}
+        public void setNombre(string nombre)
+        {
+            this.setNombre(nombre);
+        }
+        public void setEdad(int edad)
+        {
+            this.edad = edad;
+        }
+    }
+    
+    
+
+}
