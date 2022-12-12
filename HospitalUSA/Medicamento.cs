9d9d92ecce68c87810f145fcca66958a384c1782
APSMarcCliment <marc.climent@airplane.solutions>
2022-12-12 10:37:41 +0100

    Introducimos objeto Medicamento

---
+++ HospitalUSA/Medicamento.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalUSA
+{
+    public class Medicamento
+    {
+        private string nombre;
+        private string indicaciones;
+        private string ef_secundarios;
+        private int dosis;
+        private string ef_adversos;
+        private int precio;
+
+        public Medicamento(string nombre, string indicaciones, string ef_secundarios, int dosis, string ef_adversos, int precio)
+        {
+            this.nombre = nombre;
+            this.indicaciones = indicaciones;
+            this.ef_secundarios = ef_secundarios;
+            this.dosis = dosis;
+            this.ef_adversos= ef_adversos;
+            this.precio = precio;
+        }
+
+        public Medicamento() { }
+
+        public string getNombre() { return nombre;}
+        public string getIndicaciones() { return indicaciones;}
+        public string getEf_secundarios() { return ef_secundarios;}
+        public int getDosis() { return dosis;}
+        public string getEf_adversos() { return ef_adversos; }
+        public int getPrecio() { return precio;}
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void setIndicaciones(string indicaciones)
+        {
+            this.indicaciones=indicaciones;
+        }
+        public void setEf_secundarios(string ef_secundarios)
+        {
+            this.ef_secundarios=ef_secundarios;
+        }
+        public void setDosis(int dosis)
+        {
+            this.dosis = dosis;
+        }
+        public void setEf_adversos(string ef_adversos)
+        {
+            this.ef_adversos=ef_adversos;
+        }
+        public void setPrecio(int precio)
+        {
+            this.precio = precio;
+        }
+    }
+}
--- HospitalUSA/Program.cs
+++ HospitalUSA/Program.cs
@@ -31,6 +31,13 @@ namespace HospitalUSA
             pruebas.Add("Escaner");
             Paciente paciente = new Paciente();
             List<Paciente> pacientesList = new List<Paciente>();
+            
+            Medicamento medicamento1 = new Medicamento(medicacion[0], "No uso para menores de 5 años", "Adormecimiento", 3, "Dolor de barriga", 15);
+            Medicamento medicamento2 = new Medicamento(medicacion[1], "No uso para menores de 18 años", "Menor dolor de cuello", 2, "Dolor de cabeza", 10);
+            Medicamento medicamento3 = new Medicamento(medicacion[2], "No uso para menores de 3 años", "Cansancio", 4, "Dolor de huesos", 11);
+            Medicamento medicamento4 = new Medicamento(medicacion[3], "No uso para menores de 10 años", "Adormecimiento", 2, "Dolor de cuello", 18);
+            Medicamento medicamento5 = new Medicamento(medicacion[4], "No uso para menores de 10 años", "Crecimiento del pelo", 1, "Desestabilizacion hormonal", 5);
+
 
             //Persona persona1 = new Persona("Marc", 25, 74839264, "Carrer del Gall");
             //Persona persona2 = new Persona("Xavi", 27, 79289354, "Carrer del Llastics");
@@ -66,9 +73,11 @@ namespace HospitalUSA
                         paciente.setCurado(false);
                         break;
                     case 4:
+                        Console.WriteLine("Prueba que quieres hacer?");
                         paciente.setPruebas(pruebas);
                         break;
                     case 5:
+                        Console.WriteLine("Que medicacion debe tomar?");
                         paciente.setMedicacion(medicacion);
                         break;
                     case 0:
